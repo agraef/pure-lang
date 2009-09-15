1cf90d9f6c90957df0d685e9dade1012e4d8b8e7
aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f <aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f>
2009-09-15 22:54:20 +0000

    Fixes for compatibility with old Gnumeric versions.

--- gnumeric-pure/pure-loader/pure-loader.h
+++ gnumeric-pure/pure-loader/pure-loader.h
@@ -5,6 +5,9 @@
 
 #if GNM_VERSION_EPOCH <= 1 && GNM_VERSION_MAJOR <= 9 && GNM_VERSION_MINOR < 12
 #define OLD_API
+#ifndef GNM_VERSION_FULL
+#define GNM_VERSION_FULL "<unknown>"
+#endif
 #endif
 
 #include <glib-object.h>
