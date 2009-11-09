9294dfd527a657ee1e6eb3d0a180ea57d95c93bf
aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f <aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f>
2009-11-09 21:15:46 +0000

    Addon modules: license changes.

--- pd-pure/pure.c
+++ pd-pure/pure.c
@@ -1,20 +1,6 @@
 
-/* Copyright (c) 2009 by Albert Graef <Dr.Graef@t-online.de>.
-
-   This file is part of the Pure programming language and system.
-
-   Pure is free software: you can redistribute it and/or modify it under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation, either version 3 of the License, or (at your option) any later
-   version.
-
-   Pure is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-   details.
-
-   You should have received a copy of the GNU General Public License along
-   with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* Copyright (c) 2009 by Albert Graef <Dr.Graef@t-online.de>.  Distributed
+   under the new BSD license, see the accompanying COPYING file for details. */
 
 #include <stdlib.h>
 #include <stdio.h>
--- pure-xml/xml.c
+++ pure-xml/xml.c
@@ -1,20 +1,18 @@
 
 /* Copyright (c) 2009 by Albert Graef <Dr.Graef@t-online.de>.
 
-   This file is part of the Pure programming language and system.
+   pure-xml is free software: you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
 
-   Pure is free software: you can redistribute it and/or modify it under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation, either version 3 of the License, or (at your option) any later
-   version.
+   pure-xml is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
 
-   Pure is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-   details.
-
-   You should have received a copy of the GNU General Public License along
-   with this program.  If not, see <http://www.gnu.org/licenses/>. */
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 /* system headers */
 
