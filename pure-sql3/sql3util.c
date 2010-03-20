45e9d583b52c7f15da8a9b81d33855ec7209e69a
aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f <aggraef@d7baec64-8d32-11dd-a14b-9ba93c563a2f>
2010-03-20 14:57:59 +0000

    Add support for SQL function callbacks.

--- pure-sql3/sql3util.c
+++ pure-sql3/sql3util.c
@@ -26,7 +26,6 @@ sqlite3* sql3util_open(char* path) {
 
 pure_expr* sql3util_prepare(sqlite3 *db, char *sql)
 {
-  size_t size;
   sqlite3_stmt* stmtp;
   const char* zTail;
   size_t maxlen = strlen(sql) + 1;
@@ -116,7 +115,7 @@ int sql3util_bind_value(sqlite3_stmt* stm, int col, pure_expr *x)
     mpz_clear(zv);
     return sqlite3_bind_int64(stm, col, pure_get_int64(x));
   } else if (pure_is_double(x, &dv))
-    sqlite3_bind_double(stm, col, dv);
+    return sqlite3_bind_double(stm, col, dv);
   else if (pure_is_string(x, &sv))
     return sqlite3_bind_text(stm, col, sv, -1, SQLITE_TRANSIENT);
   else if (pure_is_tuplev(x, &n, NULL) && n==2) {
@@ -133,8 +132,8 @@ int sql3util_bind_value(sqlite3_stmt* stm, int col, pure_expr *x)
   } else if (pure_is_sqlnull(x))
     return sqlite3_bind_null(stm, col);
   else {
-    pure_throw(pure_app(pure_quoted_symbol(pure_sym("sql3::bad_sql_value")),
-			x));
+    pure_throw
+      (pure_app(pure_quoted_symbol(pure_sym("sql3::bad_sql_value")), x));
     return 0;
   }
 }
@@ -164,4 +163,190 @@ void sql3util_result_value(sqlite3_context* ctx, pure_expr *x)
     sqlite3_result_null(ctx);
   else
     sqlite3_result_error(ctx, "bad result type", -1);
+}
+
+/* Helper functions for managing callbacks. 2010-03-20 AG. */
+
+typedef struct {
+  int nargs;
+  pure_expr *a, *x, *y;
+  bool except;
+} cbdata;
+
+/* C wrapper around a Pure callback. This is used for scalar functions. */
+
+static void function_cb(sqlite3_context *ctx, int nargs, sqlite3_value **args)
+{
+  cbdata *cb = (cbdata*)sqlite3_user_data(ctx);
+  if (cb && cb->x) {
+    int i;
+    pure_expr *x = cb->x, *e = NULL;
+    if (cb->nargs > 0) {
+      pure_expr **xv = malloc(cb->nargs*sizeof(pure_expr*));
+      if (!xv) {
+	sqlite3_result_error(ctx, "[pure] memory overflow", -1);
+	return;
+      }
+      for (i = 0; i < nargs; i++)
+	xv[i] = sql3util_value(args[i]);
+      x = pure_appxv(x, cb->nargs, xv, &e);
+      free(xv);
+    } else if (cb->nargs == 0)
+      /* Zero-argument function, pass a dummy () argument. */
+      x = pure_appx(x, pure_tuplel(0), &e);
+    /* Variadic function, pass a single list with all the arguments. */
+    else if (nargs <= 0)
+      x = pure_appx(x, pure_listl(0), &e);
+    else {
+      pure_expr **xv = malloc(nargs*sizeof(pure_expr*));
+      if (!xv) {
+	sqlite3_result_error(ctx, "[pure] memory overflow", -1);
+	return;
+      }
+      for (i = 0; i < nargs; i++)
+	xv[i] = sql3util_value(args[i]);
+      x = pure_appx(x, pure_listv(nargs, xv), &e);
+      free(xv);
+    }
+    if (x) {
+      sql3util_result_value(ctx, x);
+      pure_freenew(x);
+    } else {
+      /* Pure exception. We handle this here so that we can report an error to
+	 the SQLite engine. */
+      if (e) pure_free(e);
+      sqlite3_result_error(ctx, "[pure] exception", -1);
+    }
+  } else
+    sqlite3_result_error(ctx, "[pure] invalid callback function", -1);
+}
+
+/* Step function in an aggregate. */
+
+static pure_expr **local_state(sqlite3_context *ctx, pure_expr *a)
+{
+  pure_expr **p = (pure_expr**)
+    sqlite3_aggregate_context(ctx, sizeof(pure_expr*));
+  if (!*p)
+    // need to initialize this instance
+    *p = pure_new(a);
+  return p;
+}
+
+static void step_cb(sqlite3_context *ctx, int nargs, sqlite3_value **args)
+{
+  cbdata *cb = (cbdata*)sqlite3_user_data(ctx);
+  if (cb && cb->x && cb->y && cb->a) {
+    int i;
+    pure_expr *x = cb->x, **a = NULL, *e = NULL;
+    if (cb->except)
+      /* Exception in previous invocation of the step function, bail out. */
+      return;
+    /* Get our local state. */
+    a = local_state(ctx, cb->a);
+    if (cb->nargs >= 0) {
+      pure_expr **xv = malloc((cb->nargs+1)*sizeof(pure_expr*));
+      if (!xv) {
+	sqlite3_result_error(ctx, "[pure] memory overflow", -1);
+	return;
+      }
+      xv[0] = *a;
+      for (i = 0; i < nargs; i++)
+	xv[i+1] = sql3util_value(args[i]);
+      x = pure_appxv(x, cb->nargs+1, xv, &e);
+      free(xv);
+    } else {
+      /* Variadic function, pass a single list with all the arguments. */
+      pure_expr **xv = malloc(nargs*sizeof(pure_expr*));
+      if (nargs > 0 && !xv) {
+	sqlite3_result_error(ctx, "[pure] memory overflow", -1);
+	return;
+      }
+      for (i = 0; i < nargs; i++)
+	xv[i] = sql3util_value(args[i]);
+      x = pure_appxl(x, &e, 2, *a, pure_listv(nargs, xv));
+      if (xv) free(xv);
+    }
+    if (x) {
+      /* Update the accumulated value. */
+      pure_expr *b = *a;
+      *a = pure_new(x);
+      pure_free(b);
+    } else {
+      /* Record an exception in a step function. */
+      if (e) pure_free(e);
+      pure_free(*a);
+      cb->except = true;
+    }
+  } else
+    sqlite3_result_error(ctx, "[pure] invalid callback function", -1);
+}
+
+/* Final function in an aggregate. */
+
+static void final_cb(sqlite3_context *ctx)
+{
+  cbdata *cb = (cbdata*)sqlite3_user_data(ctx);
+  if (cb && cb->y && cb->a) {
+    pure_expr *x = cb->y, **a = NULL, *e = NULL;
+    if (cb->except) {
+      /* Exception in previous invocation of the step function, bail out with
+	 an error. */
+      sqlite3_result_error(ctx, "[pure] exception", -1);
+      return;
+    }
+    /* Get our local state. */
+    a = local_state(ctx, cb->a);
+    x = pure_appx(x, *a, &e);
+    if (x) {
+      sql3util_result_value(ctx, x);
+      pure_freenew(x);
+    } else {
+      if (e) pure_free(e);
+      sqlite3_result_error(ctx, "[pure] exception", -1);
+    }
+    pure_free(*a);
+  } else
+    sqlite3_result_error(ctx, "[pure] invalid callback function", -1);
+}
+
+/* Passing a NULL pointer for the function argument x removes any existing
+   definition. NOTE: This leaks a tiny amount of memory for each callback
+   definition, since at present we don't keep track of the cbdata blocks
+   anywhere. This won't usually be a problem, unless your application opens an
+   arbitrary amount of database connections and register callbacks in each of
+   them, so don't do that. */
+
+int sql3util_create_function(sqlite3 *db, const char *name, int nargs,
+			     pure_expr *x)
+{
+  void *p;
+  if (pure_is_pointer(x, &p) && p == NULL)
+    return sqlite3_create_function(db, name, nargs, SQLITE_UTF8, NULL,
+				   NULL, NULL, NULL);
+  else {
+    cbdata *cb = (cbdata*)malloc(sizeof(cbdata));
+    pure_expr **xv;
+    size_t n;
+    if (!cb) return SQLITE_NOMEM;
+    cb->nargs = nargs; cb->except = false;
+    if (pure_is_tuplev(x, &n, &xv) && n>=3) {
+      /* A pair of functions (step,final) and a start value which implements
+	 an aggregate function. */
+      cb->x = pure_new(xv[0]); cb->y = pure_new(xv[1]);
+      if (n == 3)
+	cb->a = pure_new(xv[2]);
+      else
+	/* start value is a tuple */
+	cb->a = pure_new(pure_tuplev(n-2, xv+2));
+      free(xv);
+      return sqlite3_create_function(db, name, nargs, SQLITE_UTF8, cb,
+				     NULL, step_cb, final_cb);
+    } else {
+      /* A simple scalar function. */
+      cb->x = pure_new(x); cb->y = cb->a = NULL;
+      return sqlite3_create_function(db, name, nargs, SQLITE_UTF8, cb,
+				     function_cb, NULL, NULL);
+    }
+  }
 }
