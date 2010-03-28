b69202ed91740bb86fd6f45bd3087a7c2bb3a23f
p.summerland@d7baec64-8d32-11dd-a14b-9ba93c563a2f <p.summerland@d7baec64-8d32-11dd-a14b-9ba93c563a2f>
2010-03-28 23:48:18 +0000

    Revised julianutil.c, added julian_demo.pure.

--- pure-sql3/julianutil.c
+++ pure-sql3/julianutil.c
@@ -8,6 +8,10 @@
    1. In contrast, Historians usually use the Julian calendar for dates
    prior to 1582-10-15.
 
+   Daylight saving time adjustments for dates before 1971 are assumed
+   to be zero. Those after 1970 are estimates that might not be
+   accurate.
+
    Please bear these factors in mind when using this module.
   
    See julian.pure and http://en.wikipedia.org/wiki/Julian_day for further
@@ -27,25 +31,28 @@ const double MAX_UNIXTIME = 2465423.5;  //2037_12_31
 const double SUB_UNIXTIME = 2451544.5;  //2000_01_01
 const double SECS_PER_DAY = 86400.0;
 
-double local_offset(double jd);
+double local_jd(double jd);
 static void setYMD(double jd, int* y, int *m, int *d);
-static double round3(double d);
+static void setHMS(double jd, int* h, int *m, double *s);
+static int proxy_year(int y);
+static double tm_to_jd(struct tm *tmp);
+static int proxy_year(int y);
+time_t proxy_utc_secs(double jd);
 
 /* Current time, unix epoch time conversions */
 
-//Returns LONG_MIN to indicate error
 time_t jd_to_unixtime(double jd){
-  double secs = floor( (jd-BEG_UNIXTIME) * 86400 );
+  double secs = round( (jd-BEG_UNIXTIME) * SECS_PER_DAY);
   if (secs > LONG_MIN && secs < LONG_MAX)
     return (time_t)secs;
   else {
-    printf("error in jd_to_unixtime jd: %f, secs: %f\n", jd, secs);
+    //printf("dbg::jd_to_unixtime: %f, secs: %f\n", jd, secs);
     return LONG_MIN;
   }
 }
 
 double unixtime_to_jd(time_t t){
-  return t / SECS_PER_DAY + 2440587.5;
+  return t / SECS_PER_DAY + BEG_UNIXTIME;
 }
 
 double current_jd(){
@@ -54,11 +61,7 @@ double current_jd(){
   return unixtime_to_jd(secs);
 }
 
-double local_jd(double jd){
-  return jd + local_offset(jd);
-}
-
-/* Compute Julian Date from year, month. ... */
+/* jd to ymd and back family for UTC */
 
 double ymd_to_jd(int Y, int M, int D){
   int A, B, X1, X2;
@@ -74,35 +77,11 @@ double ymd_to_jd(int Y, int M, int D){
 }
 
 double ymdhms_to_jd(int Y, int M, int D, int hr, int mn, double sc){
+  //printf ("dbg::ymdhms %d %d %d  | %d %d %f\n", Y,M,D,hr,mn,sc);
   double jd = ymd_to_jd(Y,M,D);
   double secs = hr * 3600 + mn * 60 + sc;
   return jd + secs / SECS_PER_DAY;
 }
-
-double l_ymdhms_to_jd(int Y, int M, int D, int hr, int mn, double sc){
-  struct tm ltbuf;
-  time_t secs;
-  ltbuf.tm_sec = sc; //ltbuf is local time
-  ltbuf.tm_min = mn;
-  ltbuf.tm_hour = hr;
-  ltbuf.tm_mday = D;
-  ltbuf.tm_mon = M-1;
-  ltbuf.tm_year = Y + 1900;
-  ltbuf.tm_isdst = -1;
-  secs = mktime( &ltbuf ); //secs is UTC
-  if ( secs == -1 ) {
-    double local_jd = ymdhms_to_jd(Y,M,D,hr,mn,sc);    
-    return local_jd - local_offset(local_jd);
-  } else
-    return unixtime_to_jd(secs); //TODO check that this is ok
-}
-
-double l_ymd_to_jd(int Y, int M, int D){
-  return l_ymdhms_to_jd(Y, M, D, 0, 0, 0.0);
-}
-
-
-/* Return (year, month, day, ...) for Julian date */
 
 pure_expr* jd_to_ymd(double jd){
   int y, m, d;
@@ -115,8 +94,97 @@ pure_expr* jd_to_ymd(double jd){
 }
 
 pure_expr* jd_to_ymdhms(double jd){
-  int s, y, m, d, H, M; 
-  double day, day_frac, secs;
+  int y, m, d, H, M; 
+  double S;
+  setYMD(jd, &y, &m, &d);
+  setHMS(jd, &H, &M, &S);
+  pure_expr *py = pure_int(y);
+  pure_expr *pm = pure_int(m);
+  pure_expr *pd = pure_int(d);
+  pure_expr *pH = pure_int(H);
+  pure_expr *pM = pure_int(M);
+  pure_expr *pS = pure_double(S);
+  pure_expr *pret = pure_tuplel(6,py,pm,pd,pH,pM,pS);
+  return pret;  
+}
+
+/* local time functions */
+
+/* UTC jd ==> to a phoney jd that prints out local greg */
+
+double local_jd(double jd){
+  struct tm lt_tm, *ptr_tm;
+  //convert to unix time for the JD or a "similar year" within
+  //unixtime range
+  int in_unix_range = true;
+  time_t utc_secs = jd_to_unixtime(jd);
+  if (utc_secs == LONG_MIN) {
+    utc_secs = proxy_utc_secs(jd);
+    in_unix_range = false;
+  }
+  // get the local time tm struct
+  ptr_tm = localtime( &utc_secs );
+  if (ptr_tm == NULL) { //jd utc in range but local not
+    utc_secs = proxy_utc_secs(jd);
+    in_unix_range = false;
+    ptr_tm = localtime( &utc_secs );
+    if (ptr_tm == NULL) return jd;
+  }
+  memcpy( &lt_tm, ptr_tm, sizeof(struct tm) );
+  
+  // build local jd from local tm struct
+  double local_jd = tm_to_jd(&lt_tm);
+  if (in_unix_range) return local_jd;
+  double proxy_jd = unixtime_to_jd(utc_secs);
+  return jd + (local_jd - proxy_jd);
+}
+
+/* local greg tuple ==> UTC jd. */
+double l_ymdhms_to_jd(int Y, int M, int D, int hr, int mn, double sc){
+  struct tm ltbuf;
+  time_t secs;
+  ltbuf.tm_sec = sc; //ltbuf is local time
+  ltbuf.tm_min = mn;
+  ltbuf.tm_hour = hr;
+  ltbuf.tm_mday = D;
+  ltbuf.tm_mon = M-1;
+  ltbuf.tm_year = Y - 1900;
+  ltbuf.tm_isdst = -1;
+  secs = mktime( &ltbuf ); //secs is UTC
+  if ( secs != -1 ) {
+    //was within unix_range so UTC secs is valid
+    return unixtime_to_jd(secs);
+  } else {
+    //was out of unix time range, use proxy_year to get offset
+    double utc_jd = ymdhms_to_jd(Y,M,D,hr,mn,sc);
+    int py = proxy_year(Y);
+    //avoid imputed daylight saving adjustments for early years
+    int pm = py <= 1972 ? 12 : M;
+    int pd = py <= 1972 ? 31 : D;
+    double proxy_loc_jd = l_ymdhms_to_jd(py,pm,pd,hr,mn,sc);
+    double proxy_utc_jd = ymdhms_to_jd(py,pm,pd,hr,mn,sc);
+    return utc_jd + (proxy_loc_jd - proxy_utc_jd);
+  }
+}
+
+double l_ymd_to_jd(int Y, int M, int D){
+  return l_ymdhms_to_jd(Y, M, D, 0, 0, 0.0);
+}
+
+/* UTC jd ==> local greg */
+pure_expr* l_jd_to_ymd(double jd){
+  return jd_to_ymd( local_jd(jd) );
+}
+
+pure_expr* l_jd_to_ymdhms(double jd){
+  return jd_to_ymdhms( local_jd(jd) );
+}
+
+
+/* Helper functions */
+
+static void setHMS(double jd, int *hours, int *mins, double *secs){
+  double day, day_frac;
   day_frac = modf(jd, &day);
   if (day_frac < 0.5) {
     day_frac = day_frac + 0.5;
@@ -126,35 +194,15 @@ pure_expr* jd_to_ymdhms(double jd){
     day_frac = day_frac - 0.5;
     day += 0.5;
   }
-  secs =  86400.0 * day_frac;
-  s = (int) secs;
-  secs -= s;
-  H = s/3600;  //TODO CHANGE TO AVOID 59:60.000
-  s -= H*3600;
-  M = s/60;
-  s -= M*60;
-  secs = round3(secs + s);
-  setYMD(day, &y, &m, &d);
-  pure_expr *py = pure_int(y);
-  pure_expr *pm = pure_int(m);
-  pure_expr *pd = pure_int(d);
-  pure_expr *pH = pure_int(H);
-  pure_expr *pM = pure_int(M);
-  pure_expr *pS = pure_double(secs);
-  pure_expr *pret = pure_tuplel(6,py,pm,pd,pH,pM,pS);
-  return pret;  
+  double total_secs = SECS_PER_DAY * day_frac;
+  int round_secs = (int)round(total_secs);
+  double sec_frac = total_secs - round_secs;
+  if (fabs(sec_frac) < 0.0001) sec_frac = 0.0;
+  *secs = (round_secs % 60) + sec_frac;
+  int day_min = round_secs / 60;
+  *mins = day_min % 60;
+  *hours = day_min / 60;
 }
-
-pure_expr* l_jd_to_ymd(double jd){
-  return jd_to_ymd(jd + local_offset(jd));
-}
-
-pure_expr* l_jd_to_ymdhms(double jd){
-  return jd_to_ymdhms(jd + local_offset(jd));
-}
-
-
-/* Helper functions */
 
 static void setYMD(double jd, int* y, int *m, int *d){
   int Z, A, B, C, D, E, M, X1;
@@ -174,25 +222,47 @@ static void setYMD(double jd, int* y, int *m, int *d){
   *y = M>2 ? C - 4716 : C - 4715;
 }
 
-/* Returns the number of days to add to a JD to get a "local JD". The
-   jd_to_ymd[hms] functions, when applied to a local JD, return the
-   local y,m,... rather than the UTC y,m,... */
-
-double local_offset(double jd){
-  struct tm ut, *ptr_tm; 
-  time_t rawtime = jd_to_unixtime(jd);
-  if (rawtime == LONG_MIN)
-    rawtime = jd_to_unixtime(SUB_UNIXTIME);
-  ptr_tm = gmtime( &rawtime );
-  if (ptr_tm == NULL) return 0.0;
-  memcpy( &ut, ptr_tm, sizeof(struct tm) );
-  ut.tm_isdst = -1;
-  time_t utctime = mktime(&ut);
-  //TO DO if crossing time zone need to adjust.
-  return difftime(rawtime, utctime) / SECS_PER_DAY;
+static double tm_to_jd(struct tm *tmp){
+  return ymdhms_to_jd(
+    tmp->tm_year + 1900,
+    tmp->tm_mon+1,
+    tmp->tm_mday,
+    tmp->tm_hour,
+    tmp->tm_min,
+    tmp->tm_sec);
 }
 
-
-double round3(double d){
-  return round(d*1000.0)/1000.0;
+static int proxy_year(int y){
+  int leap_year = false;
+  int py = 0;
+  if (y % 400 == 0)
+    leap_year = true;
+  else if (y % 100 == 0)
+    leap_year = false;
+  else if (y % 4 == 0)
+    leap_year = true;
+  // daylight saving time shifts around, so pick accordingly
+  if (y <= 1972) {
+    py = 1972;
+  } else {
+    if (leap_year)
+      py = 2036;
+    else
+      py = 2035;
+  }
+  return py;
 }
+
+time_t proxy_utc_secs(double jd){
+  int y,m,d;
+  double proxy_jd;
+  setYMD(jd, &y, &m, &d);
+  int py = proxy_year(y);
+  //avoid daylight saving adjustment for early years 
+  if (py <= 1972)
+    proxy_jd = ymd_to_jd(py,12,31);
+  else
+    proxy_jd = ymd_to_jd(py,m,d);
+  return jd_to_unixtime(proxy_jd);
+}
+
