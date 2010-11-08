9c790369783edf679e3edec22be4813f04768c4d
Albert Graef <Dr.Graef@t-online.de>
2010-11-08 10:45:12 +0100

    Overhaul of build system; add install, update and dist targets, and a staging area for online docs.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -28,7 +28,7 @@ import sys, os
 extensions = ['sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
