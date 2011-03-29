32900c22f86e17478e2183da5ecd2f772728965a
Albert Graef <Dr.Graef@t-online.de>
2011-03-29 04:46:09 +0200

    CC and CXX env variables set compiler used by pure -c.

--- pure/interpreter.cc
+++ pure/interpreter.cc
@@ -8740,6 +8740,11 @@ int interpreter::compiler(string out, list<string> libnames)
   // Compile and link, if requested.
   if (target != out) {
     assert(bc_target);
+    string cc = "gcc";
+    string cxx = "g++";
+    const char *env;
+    if ((env = getenv("CC"))) cc = env;
+    if ((env = getenv("CXX"))) cxx = env;
     bool vflag = (verbose&verbosity::compiler) != 0;
     string libs;
     set<string> libset;
@@ -8766,7 +8771,7 @@ int interpreter::compiler(string out, list<string> libnames)
     if (WIFEXITED(status) && WEXITSTATUS(status) == 0 && ext!=".s") {
       // Assemble.
       string obj = (ext==".o")?out:out+".o";
-      cmd = "gcc -c "+quote(asmfile)+" -o "+quote(obj);
+      cmd = cc+" -c "+quote(asmfile)+" -o "+quote(obj);
       if (vflag) std::cerr << cmd << '\n';
       status = system(cmd.c_str());
       unlink(asmfile.c_str());
@@ -8786,7 +8791,7 @@ int interpreter::compiler(string out, list<string> libnames)
 	  " -lpure";
 	if (ext != ".o") {
 	  // Link.
-	  cmd = "g++ -o "+quote(out)+" "+quote(libdir)+"pure_main.o "+linkopts;
+	  cmd = cxx+" -o "+quote(out)+" "+quote(libdir)+"pure_main.o "+linkopts;
 	  if (vflag) std::cerr << cmd << '\n';
 	  status = system(cmd.c_str());
 	  unlink(obj.c_str());
