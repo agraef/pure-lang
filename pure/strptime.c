f7875e19fa3df6f2c1745d3c93c225094fe16e2c
Albert Graef <Dr.Graef@t-online.de>
2011-04-09 09:38:58 +0200

    Cosmetic changes.

--- pure/strptime.c
+++ pure/strptime.c
@@ -585,11 +585,12 @@ strptime_internal (rp, fmt, tm, decided, era_cnt LOCALE_PARAM)
               *decided = raw;
             }
 #endif
-          if (!match_string (HERE_AM_STR, rp))
+          if (!match_string (HERE_AM_STR, rp)) {
             if (match_string (HERE_PM_STR, rp))
               is_pm = 1;
             else
               return NULL;
+	  }
           break;
         case 'r':
 #ifdef _NL_CURRENT
