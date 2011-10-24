d7491f24e6d42a1a682460fd12b0c8a7a839937e
Albert Graef <Dr.Graef@t-online.de>
2011-10-24 23:05:35 +0200

    Add multimaps, various smaller improvements.

--- pure-hashmap/hashmap.cc
+++ pure-hashmap/hashmap.cc
@@ -1,6 +1,6 @@
 
 // This uses unordered_map, so a recent STL is required for now.
-// TODO: hash policy operations, multimap interface.
+// TODO: hash policy operations.
 
 #include <assert.h>
 #include <stdlib.h>
@@ -9,6 +9,12 @@
 #include <unordered_map>
 
 using namespace std;
+
+// Enable this for some additional (possibly costly) assertions in the code.
+//#define DEBUG 1
+
+// Enable this if your C++ library has the std::is_permutation function.
+//#define HAVE_STD_IS_PERMUTATION 1
 
 // Hashing and comparing Pure expressions. The required functionality is in
 // the Pure runtime (hash() and same() functions, see pure/runtime.h).
@@ -31,7 +37,7 @@ typedef unordered_map<pure_expr*,pure_expr*> myhashmap;
 
 // Runtime-configurable pretty-printing support.
 
-// FIXME: This should actually be interpreter- and/or thread-local data.
+// TLD: This should actually be interpreter- and/or thread-local data.
 static int32_t hmsym = 0;
 
 extern "C" void hashmap_symbol(pure_expr *x)
@@ -282,6 +288,16 @@ extern "C" void hashmap_del(myhashmap *m, pure_expr *key)
   }
 }
 
+extern "C" void hashmap_del2(myhashmap *m, pure_expr *key, pure_expr *val)
+{
+  myhashmap::iterator it = m->find(key);
+  if (it != m->end() && it->second && same(it->second, val)) {
+    pure_free(it->first);
+    if (it->second) pure_free(it->second);
+    m->erase(it);
+  }
+}
+
 extern "C" pure_expr *hashmap_get(myhashmap *m, pure_expr *key)
 {
   myhashmap::iterator it = m->find(key);
@@ -292,6 +308,12 @@ extern "C" bool hashmap_member(myhashmap *m, pure_expr *key)
 {
   myhashmap::iterator it = m->find(key);
   return it != m->end();
+}
+
+extern "C" bool hashmap_member2(myhashmap *m, pure_expr *key, pure_expr *val)
+{
+  myhashmap::iterator it = m->find(key);
+  return it != m->end() && it->second && same(it->second, val);
 }
 
 extern "C" bool hashmap_empty(myhashmap *m)
@@ -393,18 +415,25 @@ extern "C" void hashmap_clear(myhashmap *m)
 static bool myequal(pair<pure_expr*,pure_expr*> x,
 		    pair<pure_expr*,pure_expr*> y)
 {
-  // Should be enough to just compare the values here, as the keys are
+#ifdef DEBUG
+  // It should be enough to just compare the values here, as the keys are
   // supposed to be equal anyway.
-  //assert(same(x.first, y.first));
-  return same(x.second, y.second);
+  assert(same(x.first, y.first));
+#endif
+  if (x.second == y.second)
+    return true;
+  else if (!x.second || !y.second)
+    return false;
+  else
+    return same(x.second, y.second);
 }
 
 #include <algorithm>
 #include <tuple>
 
+#ifndef HAVE_STD_IS_PERMUTATION
 // My gcc 4.5 doesn't have this function in its C++ library, use the reference
-// implementation instead. You might wish to comment this out if your C++
-// library has this function.
+// implementation instead.
 
 template<class ForwardIterator1, class ForwardIterator2, class BinaryPredicate>
 static bool is_permutation(ForwardIterator1 first, ForwardIterator1 last,
@@ -427,15 +456,17 @@ static bool is_permutation(ForwardIterator1 first, ForwardIterator1 last,
   }
   return true;
 }
+#endif
 
 extern "C" bool hashmap_equal(myhashmap *x, myhashmap *y)
 {
+  if (x == y) return true;
   if (x->size() != y->size()) return false;
-  for (myhashmap::iterator it = x->begin(); it != x->end(); ++it) {
+  for (myhashmap::iterator it = x->begin(); it != x->end(); ) {
     /* This is probably overkill here, as unordered_map is guaranteed to have
        only one entry per key, so that the equal ranges should all have size 1.
        But we do it that way for safety, and so that the same implementation
-       can be used in the multimap case. */
+       can be used in the multimap case (see hashmmap_equal below). */
     pair<myhashmap::iterator, myhashmap::iterator>
       r1 = x->equal_range(it->first),
       r2 = y->equal_range(it->first);
@@ -443,6 +474,303 @@ extern "C" bool hashmap_equal(myhashmap *x, myhashmap *y)
       return false;
     if (!is_permutation(r1.first, r1.second, r2.first, myequal))
       return false;
+#ifdef DEBUG
+    assert(it == r1.first);
+#endif
+    it = r1.second;
+  }
+  return true;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// hashed multimaps: This is basically the same as above, with some minor
+// adjustments for the multimap implementation.
+
+//////////////////////////////////////////////////////////////////////////////
+
+typedef unordered_multimap<pure_expr*,pure_expr*> myhashmmap;
+
+// TLD: This should actually be interpreter- and/or thread-local data.
+static int32_t hmmsym = 0;
+
+extern "C" void hashmmap_symbol(pure_expr *x)
+{
+  int32_t f;
+  if (pure_is_symbol(x, &f) && f>0) hmmsym = f;
+}
+
+extern "C" pure_expr *hashmmap_list(myhashmmap *m);
+
+static const char *hashmmap_str(myhashmmap *m)
+{
+  static char *buf = 0; // TLD
+  if (buf) free(buf);
+  int32_t fsym = hmmsym?hmmsym:pure_sym("hashmmap");
+  pure_expr *f = pure_const(fsym), *x = pure_applc(f, hashmmap_list(m));
+  buf = str(x);
+  pure_freenew(x);
+  if (hmmsym && pure_sym_other(hmmsym)) {
+    const char *s = pure_sym_pname(hmmsym),
+      *t = pure_sym_pname(pure_sym_other(hmmsym));
+    size_t k = strlen(s), l = strlen(t), m = strlen(buf);
+    if (strncmp(buf, s, k) || strncmp(buf+m-l, t, l)) {
+      free(buf); buf = 0;
+      return 0;
+    }
+    char *p = buf+k, *q = buf+m-l;
+    while (p < buf+m && *p == ' ') p++;
+    while (q > buf && *--q == ' ') ;
+    if (p >= q || *p != '[' || *q != ']') {
+      free(buf); buf = 0;
+      return 0;
+    }
+    memmove(q, q+1, buf+m-q);
+    memmove(p, p+1, buf+m-p);
+  }
+  return buf;
+}
+
+static int hashmmap_prec(myhashmmap *m)
+{
+  if (hmmsym) {
+    int32_t p = pure_sym_nprec(hmmsym);
+    if (p%10 == OP_PREFIX || p%10 == OP_POSTFIX || pure_sym_other(hmmsym))
+      return p;
+    else
+      return NPREC_APP;
+  } else
+    return NPREC_APP;
+}
+
+extern "C" int hashmmap_tag(void)
+{
+  static int t = 0;
+  if (!t) {
+    t = pure_pointer_tag("hashmmap*");
+    pure_pointer_add_printer(t, (pure_printer_fun)hashmmap_str,
+			     (pure_printer_prec_fun)hashmmap_prec);
+  }
+  return t;
+}
+
+extern "C" void hashmmap_free(myhashmmap *m)
+{
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it) {
+    pure_free(it->first);
+    if (it->second) pure_free(it->second);
+  }
+  delete m;
+}
+
+static pure_expr *make_hashmmap(myhashmmap *m)
+{
+  return pure_sentry(pure_symbol(pure_sym("hashmmap_free")),
+                     pure_tag(hashmmap_tag(), pure_pointer(m)));
+}
+
+extern "C" void hashmmap_add(myhashmmap *m, pure_expr *key);
+extern "C" void hashmmap_add2(myhashmmap *m, pure_expr *key, pure_expr *val);
+
+extern "C" pure_expr *hashmmap(pure_expr *xs)
+{
+  size_t n;
+  pure_expr **xv;
+  if (!pure_is_listv(xs, &n, &xv) &&
+      !pure_is_symbolic_vectorv(xs, &n, &xv) &&
+      !(pure_is_tuplev(xs, &n, 0) && n != 1 &&
+	pure_is_tuplev(xs, &n, &xv)))
+    return 0;
+  int32_t fno = pure_getsym("=>"), gno;
+  assert(fno > 0);
+  myhashmmap *m = new myhashmmap;
+  for (size_t i = 0; i < n; i++) {
+    pure_expr *f, *g, *key, *val;
+    if (pure_is_app(xv[i], &f, &val) && pure_is_app(f, &g, &key) &&
+	pure_is_symbol(g, &gno) && gno == fno)
+      hashmmap_add2(m, key, val);
+    else
+      hashmmap_add(m, xv[i]);
+  }
+  if (xv) free(xv);
+  return make_hashmmap(m);
+}
+
+extern "C" void hashmmap_add(myhashmmap *m, pure_expr *key)
+{
+  m->insert(make_pair(pure_new(key), (pure_expr*)0));
+}
+
+extern "C" void hashmmap_add2(myhashmmap *m, pure_expr *key, pure_expr *val)
+{
+  m->insert(make_pair(pure_new(key), pure_new(val)));
+}
+
+extern "C" void hashmmap_del(myhashmmap *m, pure_expr *key)
+{
+  myhashmmap::iterator it = m->find(key);
+  if (it != m->end()) {
+    pure_free(it->first);
+    if (it->second) pure_free(it->second);
+    m->erase(it);
+  }
+}
+
+extern "C" void hashmmap_del2(myhashmmap *m, pure_expr *key, pure_expr *val)
+{
+  pair<myhashmmap::iterator, myhashmmap::iterator> r = m->equal_range(key);
+  for (myhashmmap::iterator it = r.first; it != r.second; ++it)
+    if (it->second && same(it->second, val)) {
+      pure_free(it->first);
+      if (it->second) pure_free(it->second);
+      m->erase(it);
+      return;
+    }
+}
+
+extern "C" pure_expr *hashmmap_get(myhashmmap *m, pure_expr *key)
+{
+  pair<myhashmmap::iterator, myhashmmap::iterator> r = m->equal_range(key);
+  size_t i = 0, n = distance(r.first, r.second);
+  int32_t fno = pure_getsym("=>");
+  assert(fno > 0);
+  pure_expr **xs = new pure_expr*[n], *f = pure_new(pure_symbol(fno));
+  for (myhashmmap::iterator it = r.first; it != r.second; ++it)
+    xs[i++] = it->second?pure_appl(f, 2, it->first, it->second):it->first;
+  pure_expr *x = pure_listv(n, xs);
+  delete[] xs;
+  pure_free(f);
+  return x;
+}
+
+extern "C" bool hashmmap_member(myhashmmap *m, pure_expr *key)
+{
+  myhashmmap::iterator it = m->find(key);
+  return it != m->end();
+}
+
+extern "C" bool hashmmap_member2(myhashmap *m, pure_expr *key, pure_expr *val)
+{
+  pair<myhashmmap::iterator, myhashmmap::iterator> r = m->equal_range(key);
+  for (myhashmmap::iterator it = r.first; it != r.second; ++it)
+    if (it->second && same(it->second, val))
+      return true;
+  return false;
+}
+
+extern "C" bool hashmmap_empty(myhashmmap *m)
+{
+  return m->empty();
+}
+
+extern "C" int hashmmap_size(myhashmmap *m)
+{
+  return m->size();
+}
+
+extern "C" pure_expr *hashmmap_keys(myhashmmap *m)
+{
+  size_t i = 0, n = m->size();
+  pure_expr **xs = new pure_expr*[n];
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it)
+    xs[i++] = it->first;
+  pure_expr *x = pure_listv(n, xs);
+  delete[] xs;
+  return x;
+}
+
+extern "C" pure_expr *hashmmap_vals(myhashmmap *m)
+{
+  size_t i = 0, n = m->size();
+  pure_expr **xs = new pure_expr*[n];
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it)
+    xs[i++] = it->second?it->second:it->first;
+  pure_expr *x = pure_listv(n, xs);
+  delete[] xs;
+  return x;
+}
+
+extern "C" pure_expr *hashmmap_list(myhashmmap *m)
+{
+  size_t i = 0, n = m->size();
+  int32_t fno = pure_getsym("=>");
+  assert(fno > 0);
+  pure_expr **xs = new pure_expr*[n], *f = pure_new(pure_symbol(fno));
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it)
+    xs[i++] = it->second?pure_appl(f, 2, it->first, it->second):it->first;
+  pure_expr *x = pure_listv(n, xs);
+  delete[] xs;
+  pure_free(f);
+  return x;
+}
+
+extern "C" pure_expr *hashmmap_tuple(myhashmmap *m)
+{
+  size_t i = 0, n = m->size();
+  int32_t fno = pure_getsym("=>");
+  assert(fno > 0);
+  pure_expr **xs = new pure_expr*[n], *f = pure_new(pure_symbol(fno));
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it)
+    xs[i++] = it->second?pure_appl(f, 2, it->first, it->second):it->first;
+  pure_expr *x = pure_tuplev(n, xs);
+  delete[] xs;
+  pure_free(f);
+  return x;
+}
+
+extern "C" pure_expr *hashmmap_vector(myhashmmap *m)
+{
+  size_t i = 0, n = m->size();
+  int32_t fno = pure_getsym("=>");
+  assert(fno > 0);
+  pure_expr **xs = new pure_expr*[n], *f = pure_new(pure_symbol(fno));
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it)
+    xs[i++] = it->second?pure_appl(f, 2, it->first, it->second):it->first;
+  pure_expr *x = pure_symbolic_vectorv(n, xs);
+  delete[] xs;
+  pure_free(f);
+  return x;
+}
+
+extern "C" myhashmmap *hashmmap_copy(myhashmmap *m)
+{
+  myhashmmap *m2 = new myhashmmap(*m);
+  for (myhashmmap::iterator it = m2->begin(); it != m2->end(); ++it) {
+    pure_new(it->first); if (it->second) pure_new(it->second);
+  }
+  return m2;
+}
+
+extern "C" void hashmmap_clear(myhashmmap *m)
+{
+  for (myhashmmap::iterator it = m->begin(); it != m->end(); ++it) {
+    pure_free(it->first);
+    if (it->second) pure_free(it->second);
+  }
+  m->clear();
+}
+
+extern "C" bool hashmmap_equal(myhashmmap *x, myhashmmap *y)
+{
+  if (x == y) return true;
+  if (x->size() != y->size()) return false;
+  for (myhashmmap::iterator it = x->begin(); it != x->end(); ) {
+    pair<myhashmmap::iterator, myhashmmap::iterator>
+      r1 = x->equal_range(it->first),
+      r2 = y->equal_range(it->first);
+    if (distance(r1.first, r1.second) != distance(r2.first, r2.second))
+      return false;
+    if (!is_permutation(r1.first, r1.second, r2.first, myequal))
+      return false;
+#ifdef DEBUG
+    // We assume that equal_range always yields the first element for a given
+    // key in the global traversal order, so that we can just advance to the
+    // end of the range afterwards. I couldn't find anything about this in the
+    // standard, but the assumption seems reasonable and makes the traversal
+    // more efficient.
+    assert(it == r1.first);
+#endif
+    it = r1.second;
   }
   return true;
 }
