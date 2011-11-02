df30375b6f753901d29e3aeca63932591bc009a8
Albert Graef <Dr.Graef@t-online.de>
2011-11-02 20:57:04 +0100

    Fix in orddict update operation for compatibility with stdlib dict.

--- pure-stldict/orddict.cc
+++ pure-stldict/orddict.cc
@@ -333,24 +333,24 @@ extern "C" void orddict_add(myorddict *m, pure_expr *key)
 {
   myorddict::iterator it = m->find(key);
   if (it != m->end()) {
+    pure_free(it->first);
     if (it->second) pure_free(it->second);
-    it->second = 0;
-  } else {
-    pure_new(key);
-    (*m)[key] = 0;
+    m->erase(it);
   }
+  pure_new(key);
+  (*m)[key] = 0;
 }
 
 extern "C" void orddict_add2(myorddict *m, pure_expr *key, pure_expr *val)
 {
   myorddict::iterator it = m->find(key);
   if (it != m->end()) {
+    pure_free(it->first);
     if (it->second) pure_free(it->second);
-    it->second = pure_new(val);
-  } else {
-    pure_new(key);
-    (*m)[key] = pure_new(val);
+    m->erase(it);
   }
+  pure_new(key);
+  (*m)[key] = pure_new(val);
 }
 
 extern "C" void orddict_del(myorddict *m, pure_expr *key)
