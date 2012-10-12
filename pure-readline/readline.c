e2c98740f11002393b5396b0953c7541442f00cd
Albert Graef <Dr.Graef@t-online.de>
2012-10-12 10:45:44 +0200

    Bugfix: Keep the pure-readline status separate from the interpreter.

--- pure-readline/readline.c
+++ pure-readline/readline.c
@@ -7,6 +7,7 @@
    without any warranty. */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* You might have to edit this if readline/editline lives elsewhere on your
    system. */
@@ -19,7 +20,63 @@
 
 #include <pure/runtime.h>
 
+static HISTORY_STATE *my_hist = NULL;
+
+static void my_init_history(void)
+{
+  my_hist = history_get_history_state();
+  /* KLUDGE: At this point, history_get_history_state() may return the most
+     recent interpreter history instead of an empty one, so null it out. */
+  if (my_hist->entries) {
+    my_hist->entries = NULL;
+    my_hist->offset = 0;
+    my_hist->length = 0;
+    my_hist->size = 0;
+    my_hist->flags = 0;
+  }
+}
+
 extern pure_expr *wrap_readline(const char *prompt)
 {
-  return pure_cstring(readline(prompt));
+  pure_expr *res;
+  const char *save_rl_readline_name = rl_readline_name;
+  const char *save_rl_basic_word_break_characters =
+    rl_basic_word_break_characters;
+  rl_completion_func_t *save_rl_attempted_completion_function =
+    rl_attempted_completion_function;
+  HISTORY_STATE *save_hist = history_get_history_state();
+  int histmax = unstifle_history();
+  rl_readline_name = NULL;
+  rl_basic_word_break_characters = " \t\n\"\\'`@$><=,;!|&{([";
+  rl_attempted_completion_function = NULL;
+  if (!my_hist)
+    /* Create a new (and empty) history. */
+    my_init_history();
+  history_set_history_state(my_hist);
+  res = pure_cstring(readline(prompt));
+  if (my_hist) free(my_hist);
+  my_hist = history_get_history_state();
+  rl_readline_name = save_rl_readline_name;
+  rl_basic_word_break_characters = save_rl_basic_word_break_characters;
+  rl_attempted_completion_function = save_rl_attempted_completion_function;
+  history_set_history_state(save_hist);
+  free(save_hist);
+  if (histmax>=0) stifle_history(histmax);
+  return res;
+}
+
+extern void wrap_add_history(const char *s)
+{
+  HISTORY_STATE *save_hist = history_get_history_state();
+  int histmax = unstifle_history();
+  if (!my_hist)
+    /* Create a new (and empty) history. */
+    my_init_history();
+  history_set_history_state(my_hist);
+  add_history(s);
+  if (my_hist) free(my_hist);
+  my_hist = history_get_history_state();
+  history_set_history_state(save_hist);
+  free(save_hist);
+  if (histmax>=0) stifle_history(histmax);
 }
