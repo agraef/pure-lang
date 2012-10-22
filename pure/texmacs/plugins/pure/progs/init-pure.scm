a00d493ab4f58e3e5326e07e0ae472c982869e24
Albert Graef <Dr.Graef@t-online.de>
2012-10-22 03:09:29 +0200

    Overhaul of init and patch search logic.

--- pure/texmacs/plugins/pure/progs/init-pure.scm
+++ pure/texmacs/plugins/pure/progs/init-pure.scm
@@ -14,10 +14,10 @@
 ;; Here are a few sample Pure sessions for TeXmacs. You might want to add
 ;; other session types as needed.
 
-;; INSTALLATION: Drop this file along with pure-input.scm and texmacs.pure
-;; into your ~/.TeXmacs/plugins/pure/progs folder (create the path if needed).
-
 ;; Configurable items. ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; NOTE: We allow these to be overridden by corresponding definitions in the
+;; user's init file. FIXME: Doesn't TeXmacs have a standard way of doing this?
 
 ;; Convenient keybindings. The toggle-session-math-input binding (Ctrl+$ by
 ;; default) provides a quick way to toggle between program/verbatim and math
@@ -30,46 +30,26 @@
 ;; Uncomment this to make math input the default when this module is loaded.
 ;;(if (not (session-math-input?)) (toggle-session-math-input))
 
-;; Additional TeXmacs-specific include path to search for Pure scripts. By
-;; default we set this to your ~/.TeXmacs/plugins/pure/progs folder.
+;; Additional TeXmacs-specific include paths to search for Pure scripts.
+(if (not (defined? 'pure-texmacs-includes))
 (define pure-texmacs-includes
-  (with texmacs-home (getenv "TEXMACS_HOME_PATH") ;; this should always be set
-	(string-join (list texmacs-home "plugins/pure/progs") "/")))
-
-;; Script file with additional Pure definitions to be preloaded by the Pure
-;; script plugin (see below at the end of this file). You might want to edit
-;; this script as needed.
-(define pure-texmacs-script
-  (string-join (list pure-texmacs-includes "texmacs.pure") "/"))
+  ;; TEXMACS_HOME_PATH and TEXMACS_PATH should always be set
+  (list (with texmacs-home (getenv "TEXMACS_HOME_PATH")
+	      (string-append texmacs-home "/plugins/pure/progs"))
+	(with texmacs-dir (getenv "TEXMACS_PATH")
+	      (string-append texmacs-dir "/plugins/pure/progs")))))
 
 ;; Scripts to be preloaded (if present) by the Pure script plugin. Filenames
-;; without a slash in them are looked for in the Pure library directory.
-
-;; NOTE: This is just an example; you should adjust this to your needs. Also
-;; note that these scripts are *only* preloaded by the Pure script plugin,
-;; since this doesn't provide a simple way to import the modules that you
-;; need. The session plugins always start up with just the Pure prelude
-;; loaded, so there you have to use Pure's 'using' declaration to load any
-;; additional modules that you need. Both the scripting plugin and the session
-;; plugins have the ~/.TeXmacs/plugins/pure/progs folder in the Pure module
-;; search path, however, so that any Pure scripts located there can be
-;; imported without any ado.
-
-(define pure-scripts
-  (list
-   ;; add any other scripts to be preloaded here
-   "reduce.pure" ;; if you want Reduce to be preloaded
-   ;; texmacs.pure script
-   (if (url-exists? pure-texmacs-script)
-       pure-texmacs-script
-       ;; fall back to a default texmacs.pure script in the Pure library
-       ;; directory, if any
-       "texmacs.pure")))
+;; without a slash in them are looked for first in the pure-texmacs-includes
+;; directories and then in the Pure library directory.
+(if (not (defined? 'pure-scripts))
+(define pure-scripts (list "reduce.pure" "texmacs.pure")))
 
 ;; Default Pure library path. This is normally auto-detected (see below), but
 ;; if the auto-detection doesn't work for you then you'll have to set this
 ;; variable to the path where your Pure library scripts are to be found.
-(define pure-default-lib-path "/usr/local/lib")
+(if (not (defined? 'pure-default-lib-path))
+(define pure-default-lib-path "/usr/local/lib"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -81,24 +61,35 @@
     (close-pipe port)
     (if (string? str) str pure-default-lib-path)))
 
-;; Check if the given script exists on the library path. Return the full
-;; script name if present, "" otherwise.
+;; Check if the given script exists on the library path or in one of the
+;; texmacs-specific paths. Return the full script name if present, ""
+;; otherwise.
 (define (pure-script-if-present name)
-  (with s (if (string-index name #\/)
-	      ;; filename contains a path designation, take as is
-	      name
-	      ;; look in the library dir
-	      (string-join (list pure-lib-path "pure" name) "/"))
-	(if (url-exists? s) s "")))
+  (if (string-index name #\/)
+      ;; filename contains a path designation, take as is
+      name
+      (let ((l (append pure-texmacs-includes
+		       (list (string-append pure-lib-path "/pure"))))
+	    (fullname ""))
+	(while (and (string-null? fullname) (nnull? l))
+	       (with s (string-append (car l) "/" name)
+		     (if (url-exists? s)
+			 (set! fullname s)))
+	       (set! l (cdr l)))
+	fullname)))
 
 ;; Convenience function to create a command running the Pure interpreter with
 ;; the given scripts (if present).
 (define (pure-cmd cmd scripts)
-  (string-join (append (list cmd)
-		       (if (string? pure-texmacs-includes)
-			   (list "-I" pure-texmacs-includes)
-			   (list))
-		       (map pure-script-if-present scripts)) " "))
+  (string-join
+   (append (list cmd)
+	   (map (lambda (s)
+		  (string-append "-I " (format #f "~s" s)))
+		pure-texmacs-includes)
+	   (map (lambda (s)
+		  (format #f "~s" (pure-script-if-present s)))
+		scripts))
+   " "))
 
 ;; Entry point for Pure help commands.
 (define (decompose-url s)
