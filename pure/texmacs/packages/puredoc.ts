d5010a27bd6ea02fda632cc924f9dd9c7ade9e8a
Albert Graef <Dr.Graef@t-online.de>
2012-11-07 22:04:12 +0100

    Comment changes.

--- pure/texmacs/packages/puredoc.ts
+++ pure/texmacs/packages/puredoc.ts
@@ -3,11 +3,37 @@
 <style|source>
 
 <\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|puredoc|0.1>
+
+      <\src-purpose>
+        Formatting of the Pure online documentation. To be used with
+        generic.ts or any other basic document style of your choice.
+      </src-purpose>
+
+      <\src-copyright|2012>
+        Albert Gräf
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
   <active*|<\src-comment>
-    puredoc style option. This makes up for some of the glitches in the
-    converted html docs: verbatim has par-par-sep set to zero, and images are
-    moved relative to the baseline. We also add article-style section
-    numbering and adjust the description environments.
+    Some convenient formatting options for the <TeXmacs>-formatted Pure
+    documentation. Specifically, the verbatim environments have par-par-sep
+    set to zero, and images are moved relative to the baseline. We also add
+    article-style section numbering and adjust the rendering of the
+    description environments to get rid of the trailing dot in the
+    description labels. This gives a pretty nice rendering of the documents,
+    but of course your preferences may vary, so you might want to adjust the
+    following definitions as needed.
   </src-comment>>
 
   <assign|orig-verbatim|<value|verbatim>>
